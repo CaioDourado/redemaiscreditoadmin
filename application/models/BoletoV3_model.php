20e4beba8d08ea1a54e05da3a63094bca5b87538
Caio Dourado <caiof.dourado@gmail.com>
2026-09-21 10:00:00 -0300

    Corrige prazo de pagamento dos boletos Sicoob

--- application/models/BoletoV3_model.php
+++ application/models/BoletoV3_model.php
@@ -121,7 +121,10 @@ class BoletoV3_model extends CI_Model{
     }
 
     private function montar_payload_sicoob($pagador, $valor, $data_vencimento, $outros, $id_boleto_atual){
-        $data_limite = date('Y-m-d',strtotime($data_vencimento.'+6 months'));
+        // O vencimento permanece o informado na fatura. O Sicoob aceita pagamento
+        // apos o vencimento somente ate 180 dias, ainda que seis meses-calendario
+        // possam ultrapassar esse limite em alguns periodos.
+        $data_limite = date('Y-m-d',strtotime($data_vencimento.' +180 days'));
         $data_multa = date('Y-m-d',strtotime($data_vencimento.'+1 day'));
         $cep = $this->safe($pagador, 'cep');
         $valid_cep = str_replace(array('.', '-'), '', $cep);
